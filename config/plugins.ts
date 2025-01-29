010a87db2a9c80bc440d7c710571ac9fbbd13e9a
Mattis SIMON <mattis.simon972+github@gmail.com>
2025-01-29 16:28:20 +0100

    [UPDATE] Token expiration date

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,6 +2,9 @@ export default ({ env }) => ({
     "users-permissions": {
         config: {
             jwtSecret: env("JWT_SECRET"),
+            jwt: {
+                expiresIn: "7d",
+            },
         },
     },
     upload: {
